dafff6f6a472621f5aa303d6c677b4386eaa1104
ChidyOmmy <Chidyommy@outlook.com>
2024-10-19 04:15:46 +0300

    add page model, create, get and modify page routes

--- routes/books.js
+++ routes/books.js
@@ -6,6 +6,7 @@ import multer from "multer";
 import mongoose from "mongoose";
 import path from "path";
 import { verifyAccesskey } from "../middleware/accessKeyVerify.js";
+import pages from "./pages.js";
 
 const books = express.Router();
 
@@ -127,13 +128,13 @@ books.put("/add-authors", async (req, res) => {
       const authorsPromises = authors.map(async (authorId) => {
         const author = await User.findById(authorId);
         if (!author) throw new Error(`${authorID} not found`);
-        if(book.authors.includes(author._id)) throw new Error(`${author.username} is already an author`)
+        if (book.authors.includes(author._id))
+          throw new Error(`${author.username} is already an author`);
         return author._id;
       });
       const authorIds = await Promise.all(authorsPromises);
       book.authors.push(...authorIds);
-    } 
-    else {
+    } else {
       try {
         const author = await User.findById(authors);
         book.authors.push(author._id);
@@ -147,62 +148,73 @@ books.put("/add-authors", async (req, res) => {
         .status(201)
         .json({ message: "Successfully added more authors", book });
     } catch (error) {
-      return res.status(500).json({error: error.message});
+      return res.status(500).json({ error: error.message });
     }
   } catch (error) {
-    return res.status(400).json({error: error.message});
+    return res.status(400).json({ error: error.message });
   }
 });
 
-books.put('/update-book',upload, async(req,res)=>{
-  const {title, subtitle, summary, bookId} = req.body
-  const authors = JSON.parse(req.body.authors)
-  
-   if (!mongoose.isValidObjectId(bookId))
+books.put("/update-book", upload, async (req, res) => {
+  const { title, subtitle, summary, bookId } = req.body;
+  const authors = JSON.parse(req.body.authors);
+
+  if (!mongoose.isValidObjectId(bookId))
     return res.status(400).json({ error: "Invalid ObjectId for Book" });
-  const book = await Book.findById(bookId)
+  const book = await Book.findById(bookId);
 
-  if(!book) return res.status(404).json({error: 'Book not found, It might have been deleted'})
-  if(title) book.title = title
-  if(subtitle) book.subtitle = subtitle
-  if(summary) book.summary = summary
-  if(req.file) book.cover = req.file.filename
+  if (!book)
+    return res
+      .status(404)
+      .json({ error: "Book not found, It might have been deleted" });
+  if (title) book.title = title;
+  if (subtitle) book.subtitle = subtitle;
+  if (summary) book.summary = summary;
+  if (req.file) book.cover = req.file.filename;
 
-  // adding authors 
+  // adding authors
 
-  if(authors.length > 0){
-     if (authors.constructor == Array) {
+  if (authors.length > 0) {
+    if (authors.constructor == Array) {
       const authorsPromises = authors.map(async (authorId) => {
-        if (!mongoose.isValidObjectId(authorId)) throw new Error(`Invalid ObjectId for user ${authorId} in array of authors`)
+        if (!mongoose.isValidObjectId(authorId))
+          throw new Error(
+            `Invalid ObjectId for user ${authorId} in array of authors`
+          );
         const author = await User.findById(authorId);
         if (!author) throw new Error(`${authorID} not found`);
-        if(book.authors.includes(author._id)) throw new Error(`${author.username} is already an author`)
+        if (book.authors.includes(author._id))
+          throw new Error(`${author.username} is already an author`);
         return author._id;
       });
       const authorIds = await Promise.all(authorsPromises);
       book.authors.push(...authorIds);
     } else {
       try {
-        if (!mongoose.isValidObjectId(authors)) throw new Error(`Invalid ObjectId for user ${authors}`)
+        if (!mongoose.isValidObjectId(authors))
+          throw new Error(`Invalid ObjectId for user ${authors}`);
         const author = await User.findById(authors);
         book.authors.push(author._id);
       } catch (error) {
         return res.status(500).json({ error: error.message });
       }
     }
-    
+
     // end of add authors
   }
-  
-   try {
-    await book.save()
-     const updatedBook = await Book.findById(book._id)
+
+  try {
+    await book.save();
+    const updatedBook = await Book.findById(book._id)
       .populate("authors comments", "username _id text")
       .exec();
-    return res.status(200).json({message: 'Successfully updated book', book: updatedBook})
-   } catch (error) {
-    return res.status(500).json({error: error.message})
-   }})
+    return res
+      .status(200)
+      .json({ message: "Successfully updated book", book: updatedBook });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
 
 books.get("/:id", async (req, res) => {
   const id = req.params.id;
@@ -216,5 +228,6 @@ books.get("/:id", async (req, res) => {
   }
 });
 
+books.use(pages);
 books.use(actions);
 export default books;
